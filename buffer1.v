290062c60b002c00a0cf6c45072fd23c529727de
aaroncruz404 <75393759+aaroncruz404@users.noreply.github.com>
2020-12-09 15:24:35 -0600

    Add files via upload

--- buffer1.v
+++ buffer1.v
@@ -4,12 +4,7 @@ input [31:0]pcsumain,
 input clk,
 output reg [31:0]intruccion_out,
 output reg [31:0]pcsumaout
-/*
-output reg [4:0]rs_BF0, rt_BF0, rd_BF0,
-output reg [5:0]op_BF0,
-output reg [15:0]rdshfunct_BF0,
-output reg [25:0]target_BF0
-*/
+
 );
 
 always @(posedge clk)
